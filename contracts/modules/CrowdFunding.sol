558b86c3b8fa615a66232b6da3bd9f7606e6fd69
1Clavero <arturoclavero01@gmail.com>
2025-07-05 21:50:48 +0700

    feat: crowdfunding deadline

--- contracts/modules/CrowdFunding.sol
+++ contracts/modules/CrowdFunding.sol
@@ -6,31 +6,62 @@ import "../utils/FundErrors.sol";
 
 /// @title Crowdfunding - Campaign contract with refund logic
 /// @notice Accepts user funds toward a target; allows refunds if cancelled
+/// @notice Optional deadlines, users can claim a refund if target is not funded by the deadline
 contract Crowdfunding is FundBase{
+	mapping (address => uint256) private contributions;
     uint256 public immutable target;
     bool public isCancelled = false;
-    mapping (address => uint256) private contributions;
+	bool public ended = false;
+	uint256 public deadline;
+	
     event CampaignCancelled();
+	event CampaignEnded();
     event RefundClaimed(address indexed by, uint256 amount);
     
-    constructor(uint256 _target) FundBase(){
-        target = _target;
-    }
+	modifier stillActive() {
+		if (isCancelled) revert CancelledAlready();
+		if (ended) revert EndedAlready();
+		if (deadline != 0 && deadline < block.timestamp) endCampaign();
+		_;
+	}
 
-    function depositFunds() public payable override{
+    constructor(
+    uint256 _target,
+    uint256 _seconds,
+    uint256 _minutes,
+    uint256 _hours,
+    uint256 _days,
+    uint256 _weeks
+	)  FundBase(){
+		target = _target;
+		deadline = _seconds * 1 seconds +
+				_minutes * 1 minutes +
+				_hours   * 1 hours +
+				_days    * 1 days +
+				_weeks   * 1 weeks;
+		if (deadline != 0) deadline += block.timestamp;
+	}
+
+    function depositFunds() public payable override stillActive{
         super.depositFunds(); 
         contributions[msg.sender] += msg.value;
     }
 
-    function withdrawFunds() public onlyOwner {
-        if (isCancelled) revert CancelledAlready();
+    function withdrawFunds() public onlyOwner stillActive{
         if (target > address(this).balance) revert GoalNotReached();
         super.withdrawFunds(address(this).balance);
     }
 
     function cancelCampaign() public onlyOwner {
+		if (isCancelled) revert CancelledAlready();
         isCancelled = true;
         emit CampaignCancelled();
+    }
+
+	function endCampaign() private {
+		ended = true;
+		if (address(this).balance < target) cancelCampaign();
+        emit CampaignEnded();
     }
 
     function claimRefund() public {
